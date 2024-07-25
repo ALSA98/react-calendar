61d666016868854d519a8b9dcdf686f332938d17
Ali Saraie <ali.saraie92@gmail.com>
2024-07-25 18:52:44 +0330

    feat: Add next and prev buttons UI

--- src/components/CalendarHeaderNavigator.tsx
+++ src/components/CalendarHeaderNavigator.tsx
@@ -1,9 +1,19 @@
 import { Button } from "@/components/ui/Button";
+import { ChevronLeftIcon } from "lucide-react";
+import { ChevronRightIcon } from "lucide-react";
 
 const CalendarHeaderNavigator = () => {
   return (
-    <div className="inline-flex h-full items-center">
+    <div className="inline-flex h-full items-center gap-3">
       <Button variant="outline">Today</Button>
+      <div>
+        <Button variant="ghost" className="px-2">
+          <ChevronLeftIcon />
+        </Button>
+        <Button variant="ghost" className="px-2">
+          <ChevronRightIcon />
+        </Button>
+      </div>
     </div>
   );
 };
