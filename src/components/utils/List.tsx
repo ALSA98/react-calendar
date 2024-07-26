bef7ac19275c9260197739cda521db1b4cc09584
Ali Saraie <ali.saraie92@gmail.com>
2024-07-26 15:02:11 +0330

    feat: Add `<List>` util component

---
+++ src/components/utils/List.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+
+type Props<T> = {
+  items: T[];
+  getKey?: (item: T, index: number) => string | number;
+  children: (item: T, key: string | number, index: number) => ReactNode;
+};
+
+const defaultGetKey = (item: any, index: number) =>
+  (item.id || item.value || item.key || index) as string | number;
+
+const List = <T,>({ items, getKey = defaultGetKey, children }: Props<T>) => {
+  return (
+    <>
+      {items.map((item, index) => {
+        const itemJSX = children(item, getKey(item, index), index);
+        return itemJSX;
+      })}
+    </>
+  );
+};
+
+export default List;
