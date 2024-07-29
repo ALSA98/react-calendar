c1da390c06ec098c1dfbb68b002741d86ca71ab5
Ali Saraie <ali.saraie92@gmail.com>
2024-07-29 21:24:14 +0330

    feat: Add `<Checkbox>` and `<CheckBoxWithLabel>`

---
+++ src/components/ui/CheckboxWithLabel.tsx
@@ -0,0 +1,30 @@
+import { ComponentProps, forwardRef, Ref, ReactNode, useId } from "react";
+import { Checkbox } from "@/components/ui/Checkbox";
+import { Label } from "@/components/ui/Label";
+import { cn } from "@/lib/utils";
+
+type Props = {
+  label: ReactNode | string;
+  labelProps?: ComponentProps<typeof Label>;
+  className?: string;
+} & ComponentProps<typeof Checkbox>;
+
+const CheckboxWithLabel = forwardRef(
+  (
+    { label, labelProps, className, ...checkboxProps }: Props,
+    ref: Ref<HTMLButtonElement>,
+  ) => {
+    const id = useId();
+
+    return (
+      <div className={cn("flex items-center space-x-2", className)}>
+        <Checkbox id={id} ref={ref} {...checkboxProps} />
+        <Label htmlFor={id} {...labelProps}>
+          {label}
+        </Label>
+      </div>
+    );
+  },
+);
+
+export default CheckboxWithLabel;
