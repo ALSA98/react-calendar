a7ad9380a6b3dcfb5a41d209c87074ee716e0886
Ali Saraie <ali.saraie92@gmail.com>
2024-08-01 13:38:09 +0330

    feat: Add logic to `<ColorPicker>`

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,14 +1,35 @@
 import { Label } from "@/components/ui/Label";
 import { Button } from "@/components/ui/Button";
 
-const ColorPicker = () => {
+import { EVENT_COLORS } from "@/context/EventContext";
+import List from "@/components/utils/List";
+import { cn } from "@/lib/utils";
+
+type Props = {
+  selected?: (typeof EVENT_COLORS)[number];
+};
+
+const ColorPicker = ({ selected }: Props) => {
+  const activeClasses =
+    "after:absolute after:size-3 after:rounded-full after:bg-background after:content-['']";
+
   return (
     <div className="flex-col space-y-2">
       <Label>Color</Label>
+
       <div className="flex space-x-2">
-        <Button size="smIcon" className="rounded-full bg-red-600" />
-        <Button size="smIcon" className="rounded-full bg-blue-600" />
-        <Button size="smIcon" className="rounded-full bg-green-600" />
+        <List items={EVENT_COLORS as unknown as string[]}>
+          {(color) => (
+            <Button
+              size="smIcon"
+              className={cn(
+                "relative rounded-full hover:opacity-75",
+                color === selected && activeClasses,
+              )}
+              style={{ backgroundColor: color }}
+            />
+          )}
+        </List>
       </div>
     </div>
   );
